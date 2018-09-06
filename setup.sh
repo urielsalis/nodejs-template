5046976188c085b21fec8e45c93dd7fc8ab64319
Uriel Salischiker <urielsalis@users.noreply.github.com>
2018-09-06 16:59:30 -0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt update
+sudo apt -y install curl python-software-properties
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt install -y nodejs
+chmod +x -R /home/ubuntu/deploy/bin
